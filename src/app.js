3771f0a228fadd10681eb2132f6827e307bd17df
ElioViottado <elioviottado@gmail.com>
2023-02-21 18:47:27 +0000

    Domain

--- src/app.js
+++ src/app.js
@@ -7,5 +7,17 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+
+  var pronoun = ["the", "our"];
+  var adj = ["great", "big"];
+  var noun = ["jogger", "racoon"];
+
+  pronoun.forEach(function(pronounItem) {
+    adj.forEach(function(adjItem) {
+      noun.forEach(function(nounItem) {
+        console.log(pronounItem + adjItem + nounItem + ".com");
+      });
+    });
+  });
   console.log("Hello Rigo from the console!");
 };
